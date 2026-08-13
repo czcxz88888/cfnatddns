2b029aa6944fe693bb3159dacd2c5e763920f1ed
onlyoyrao999 <onlyoyrao@gmail.com>
2026-08-14 04:48:55 +0800

    chore: Bump app version to 1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     applicationId = "com.aistudio.cfipscanner.app"
     minSdk = 24
     targetSdk = 36
-    versionCode = 2
-    versionName = "1.2"
+    versionCode = 3
+    versionName = "1.25"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
